728b1e6f09db4ca01cbe41c85b7ee24d198bbd92
mr.chalaki <129165551+BABAK-CHALAKI@users.noreply.github.com>
2023-09-01 02:20:25 -0700

    Update timer.js

--- METRONOME/timer.js
+++ METRONOME/timer.js
@@ -39,37 +39,4 @@ export default Timer;
 
 
 
-
-// function Timer(callback, timeInterval, options) {
-//     this.timeInterval = timeInterval;
-//     this.timeout = null;
-//     this.expected = null;
-    
-//     this.start = () => {
-//     this.expected = Date.now() + this.timeInterval;
-//     this.timeout = null;
-//     if (options && options.immediate) {
-//     callback();
-//     console.log("immediate");
-//     }
-//     this.timeout = setTimeout(this.round, this.timeInterval);
-//     };
-    
-//     this.stop = () => {
-//     clearTimeout(this.timeout);
-//     };
-    
-//     this.round = () => {
-//     let drift = Date.now() - this.expected;
-//     if (drift > this.timeInterval) {
-//     if (options && options.errorCallback) {
-//     options.errorCallback();
-//     }
-//     }
-//     callback();
-//     this.expected += this.timeInterval;
-//     this.timeout = setTimeout(this.round, this.timeInterval - drift);
-//     };
-//     }
-    
-//     export default Timer;
+
